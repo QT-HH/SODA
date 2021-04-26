db91fb911e8cf02188271b728c514d6b5f31165c
TAEJIN KIM <taejin0323@gmail.com>
2021-04-26 23:24:20 +0900

    feat: 🎸 springboot timezone && swagger ui
    swagger ui가 안 뜨는데... 	@PostConstruct

--- backend/springboot/src/main/java/com/tak/soda/config/SwaggerConfig.java
+++ backend/springboot/src/main/java/com/tak/soda/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -16,7 +18,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.consumes(getConsumeContentTypes())
+				.produces(getProduceContentTypes())
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.tak.soda.controller"))
+				.paths(PathSelectors.any())
+				.build();
+
+	}
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("Soda")
@@ -35,19 +56,5 @@ public class SwaggerConfig {
 		produces.add("application/json;charset=UTF-8");
 		return produces;
 	}
-	
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.consumes(getConsumeContentTypes())
-				.produces(getProduceContentTypes())
-				.apiInfo(apiInfo())
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.tak.soda.controller"))
-				.paths(PathSelectors.any())
-				.build();
-
-	}
-	
 }
 
