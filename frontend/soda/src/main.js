a60c870528485c6836925ddcac1220b46eaa7b4e
김유리 <youlee602@hanmail.net>
2021-04-27 16:56:32 +0900

    feat : Create register page

--- frontend/soda/src/main.js
+++ frontend/soda/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Vuetify from '@/plugins/vuetify';
-
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
 new Vue({
+	vuetify: new Vuetify(),
 	router,
 	store,
 	Vuetify,
---
+++ frontend/soda/src/views/Certify.vue
@@ -0,0 +1,43 @@
+<template>
+	<v-row justify="center">
+		<v-col cols="12" sm="10" md="8" lg="6" style="margin: auto">
+			<div>
+				<br />
+				<h2>인증코드를 입력해주세요.</h2>
+				<br />
+				<br />
+				<br />
+				<p>인증코드</p>
+				<br />
+				<div>
+					<v-text-field filled rounded dense v-model="code"></v-text-field>
+				</div>
+				<div>
+					<v-btn color="primary" text @click="certify"> 다음 </v-btn>
+				</div>
+			</div>
+		</v-col>
+	</v-row>
+</template>
+
+<script>
+export default {
+	name: 'Certify',
+	components: {},
+	data: () => ({
+		code: null,
+	}),
+	methods: {
+		certify() {
+			if (this.code === null) {
+				alert('인증코드를 입력해주세요.');
+			} else {
+				console.log(this.code);
+				// this.$router.push('/invite');  -> 초대페이지로 넘어감
+			}
+		},
+	},
+};
+</script>
+
+<style></style>
