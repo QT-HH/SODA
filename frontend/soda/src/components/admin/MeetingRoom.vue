9e4d12f91a1259f72299426cd207ee426387ee75
백규태 <hyunheal@naver.com>
2021-05-03 18:02:06 +0900

    fix: tmpSave MeetingRoom.vue

---
+++ frontend/soda/src/components/admin/MeetingRoom.vue
@@ -0,0 +1,18 @@
+<template>
+	<tr>
+		<td class="text-left">{{ Room.sessionid }}</td>
+		<td class="text-left">{{ Room.owner }}</td>
+		<td class="text-left">{{ Room.participants }}</td>
+	</tr>
+</template>
+
+<script>
+export default {
+	name: 'MeetingRoom',
+	props: {
+		Room: Object,
+	},
+};
+</script>
+
+<style></style>
