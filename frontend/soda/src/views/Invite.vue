33bc41fa39252e7134020f1fb15d41c28251f07f
김지은 <vldzmf0805@naver.com>
2021-05-03 22:44:07 +0900

    feat : Fix Invite Page(FE)

--- frontend/soda/src/views/Invite.vue
+++ frontend/soda/src/views/Invite.vue
@@ -3,56 +3,100 @@
 		<section class="bgcolor"></section>
 		<div class="contentwrap">
 			<h1 class="pb-3" id="font3">면접 참여자의 이메일을 입력해주세요.</h1>
-			<p class="content" id="font2">
+			<p class="content ma-0" id="font2">
 				* 해당 이메일로 면접 참여 코드가 전송됩니다.
 			</p>
-			<p id="font1">면접관</p>
-			<div>
-				<h2 id="font1">미팅을 시작하시겠습니까?</h2>
-				<br />
-				<h2 id="font1">초대할 이메일을 입력해주세요.</h2>
-				<br />
-				<br />
-				<p id="font1">면접관</p>
-				<div>
-					<v-text-field v-model="firemailmg"></v-text-field>
+			<p class="content" id="font2">
+				(+) 버튼을 누르셔야지 초대 목록에 추가됩니다.
+			</p>
+			<div class="section1">
+				<span id="font3" style="font-size: 20px">면접관 </span>
+				<span id="font2" style="font-size: 15px">최대 2명</span>
+				<v-row cols="12" class="pt-3">
+					<v-col cols="11">
+						<v-text-field
+							v-model="firemailmg"
+							solo
+							flat
+							rounded
+							dense
+						></v-text-field>
+					</v-col>
+					<v-col cols="1">
+						<v-btn
+							color="secondary"
+							plain
+							fab
+							x-small
+							@click="addMG(firemailmg)"
+							v-show="inviteok"
+							class="plusbtn"
+						>
+							+
+						</v-btn>
+					</v-col>
+				</v-row>
+				<div class="overflowScroll">
+					<v-chip-group column>
+						<v-chip
+							v-for="(item, index) of emailsmg"
+							v-bind:key="index"
+							color="#303F9F"
+							outlined
+							label
+							close
+							@click:close="deletemg(index)"
+							class="mb-1"
+							>{{ item }}</v-chip
+						>
+					</v-chip-group>
 				</div>
-				<div v-for="(item, index) of inputsmg" v-bind:key="index">
-					<v-text-field v-model="emailsmg[index]"></v-text-field>
+			</div>
+			<div class="section2">
+				<span id="font3" style="font-size: 20px">면접자</span>
+				<span id="font2" style="font-size: 15px">(필수) 최대 30명</span>
+				<v-row cols="12" class="pt-3">
+					<v-col cols="11">
+						<v-text-field
+							v-model="firemailmj"
+							solo
+							flat
+							rounded
+							dense
+						></v-text-field>
+					</v-col>
+					<v-col cols="1">
+						<v-btn
+							color="secondary"
+							plain
+							fab
+							x-small
+							@click="addMJ(firemailmj)"
+							v-show="inviteok2"
+							class="plusbtn"
+						>
+							+
+						</v-btn>
+					</v-col>
+				</v-row>
+				<div class="overflowScroll">
+					<v-chip-group column>
+						<v-chip
+							v-for="(item, index) of emailsmj"
+							v-bind:key="index"
+							color="#388E3C"
+							outlined
+							label
+							close
+							@click:close="deletemj(index)"
+							class="mb-1"
+							>{{ item }}</v-chip
+						>
+					</v-chip-group>
 				</div>
-				<div>
-					<v-btn
-						color="secondary"
-						fab
-						x-small
-						dark
-						@click="addMG"
-						v-show="inviteok"
-					>
-						+
-					</v-btn>
-				</div>
-				<br />
-				<br />
-				<br />
-				<br />
-				<p id="font1">면접자</p>
-				<div>
-					<v-text-field v-model="firemailmj"></v-text-field>
-				</div>
-				<div v-for="(item, index) of inputsmj" v-bind:key="index">
-					<v-text-field v-model="emailsmj[index]"></v-text-field>
-				</div>
-				<div>
-					<v-btn color="secondary" fab x-small dark @click="addMJ"> + </v-btn>
-				</div>
-				<br />
-				<br />
-				<br />
-				<br />
-				<div>
-					<v-btn color="primary" @click="startMeeting"> 시작하기 </v-btn>
-				</div>
+			</div>
+			<div class="startbtn">
+				<button id="font3" @click="startMeeting">시작하기</button>
 			</div>
 		</div>
 	</div>
@@ -68,6 +112,7 @@ export default {
 			inputsmg: 0,
 			inputsmj: 0,
 			inviteok: true,
+			inviteok2: true,
 			firemailmj: '',
 			firemailmg: '',
 			emailsmg: [],
@@ -85,22 +130,35 @@ export default {
 			'checkSessionId',
 			'changeMeetingDialog',
 		]),
-		addMG() {
-			this.inputsmg++;
-			if (this.inputsmg >= 2) this.inviteok = false;
+		addMG(email) {
+			if (email !== '') {
+				this.emailsmg.push(email);
+				if (this.emailsmg.length >= 2) this.inviteok = false;
+				this.firemailmg = '';
+			}
 		},
-		addMJ() {
-			this.inputsmj++;
+		addMJ(email) {
+			if (email !== '') {
+				this.emailsmj.push(email);
+				if (this.emailsmj.length >= 30) this.inviteok2 = false;
+				this.firemailmj = '';
+			}
+		},
+		deletemg(idx) {
+			this.emailsmg.splice(idx, 1);
+		},
+		deletemj(idx) {
+			this.emailsmj.splice(idx, 1);
 		},
 		startMeeting() {
-			if (this.firemailmj == '' || this.firemailmg) {
-				alert('이메일을 입력해주세요.');
+			if (this.firemailmj == '') {
+				alert('a이메일을 입력해주세요.');
 				return;
 			}
 			console.log(this.firemailmj);
 			for (let index = 0; index < this.emailsmj.length; index++) {
 				if (this.emailsmj[index] == '') {
-					alert('이메일을 입력해주세요.');
+					alert('b이메일을 입력해주세요.');
 					return;
 				}
 				var el = this.emailsmj[index];
@@ -109,7 +167,7 @@ export default {
 			console.log(this.firemailmg);
 			for (let index = 0; index < this.emailsmg.length; index++) {
 				if (this.emailsmg[index] == '') {
-					alert('이메일을 입력해주세요.');
+					alert('c이메일을 입력해주세요.');
 					return;
 				}
 				var el2 = this.emailsmg[index];
@@ -125,16 +183,65 @@ export default {
 .bgcolor {
 	position: relative;
 	width: 100vw;
-	height: 100vh;
+	min-height: 100vh;
 	background-color: #e0dcdd;
 }
 .contentwrap {
 	width: 600px;
-	position: fixed;
+	position: absolute;
 	top: calc(50% - 241px);
-	left: calc(50% - 250px);
+	left: calc(50% - 300px);
 }
 .content {
 	font-size: 13px;
+}
+.section1 {
+	position: fixed;
+	top: 35%;
+	left: 15vw;
+	right: 53vw;
+}
+.section2 {
+	position: fixed;
+	top: 35%;
+	left: 53vw;
+	right: 15vw;
+}
+.startbtn:hover {
+	background-color: #b71c1c;
+	color: white;
+	outline: 0;
+}
+.startbtn:focus {
+	outline: none;
+}
+.startbtn {
+	position: fixed;
+	bottom: 15vh;
+	left: calc(50% - 54px);
+	background-color: transparent;
+	border: 2px solid #b71c1c;
+	border-radius: 30px;
+	color: #b71c1c;
+	cursor: pointer;
+	font-size: 17px;
+	padding: 6px 20px;
+	transition: all 200ms;
+}
+.col {
+	padding-left: 0px;
+	padding-right: 0px;
+}
+.plusbtn {
+	padding-left: 10px;
+	padding-right: 0px;
+	padding-top: 5px;
+	font-size: 50px;
+	color: black;
+	width: 5px;
+}
+.overflowScroll {
+	height: 200px;
+	overflow-y: auto;
 }
 </style>
