fbd2af986b079e833a6899d10ed1a7f3d5f00c9d
김유리 <youlee602@hanmail.net>
2021-05-03 21:40:42 +0900

    fix: Update api

--- frontend/soda/src/api/company.js
+++ frontend/soda/src/api/company.js
@@ -7,10 +7,19 @@ function delCompany(cid) {
 	return instance.delete(`company/del/${cid}`);
 }
 function approveEmail(email, u_id) {
-	return instance.get(`company/email/approve/${email}/${u_id}`);
+	return instance.get('company/email/approve', {
+		params: {
+			email: email,
+			u_id: u_id,
+		},
+	});
 }
 function rejectEmail(email) {
-	return instance.get(`company/email/reject/${email}`);
+	return instance.get('company/email/reject', {
+		params: {
+			email: email,
+		},
+	});
 }
 function findCompany(cname) {
 	return instance.get(`company/find/${cname}`);
@@ -19,7 +28,9 @@ function companyList() {
 	return instance.get('company/list');
 }
 function newCompany(cName, email, phone, role, uName) {
-	return instance.post('company/new', cName, email, phone, role, uName);
+	return instance.post(
+		`company/new?cName=${cName}&email=${email}&phone=${phone}&role=${role}&uName=${uName}`,
+	);
 }
 
 export {
