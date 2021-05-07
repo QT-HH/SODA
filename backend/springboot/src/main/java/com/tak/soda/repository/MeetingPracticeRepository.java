7583092b20ab6bad3d9d6e8d5e17e0ac94ad4149
TAEJIN <taejin0323@naver.com>
2021-05-07 13:17:00 +0900

    feat: 🎸 MeetingPractice Repository 추가
    JpaRepository 인터페이스 사용
    
    ✅ Closes: S04P31D103-138

--- backend/springboot/src/main/java/com/tak/soda/repository/MeetingPracticeRepository.java
+++ backend/springboot/src/main/java/com/tak/soda/repository/MeetingPracticeRepository.java
@@ -1,7 +1,11 @@
 package com.tak.soda.repository;
 
+import com.tak.soda.domain.MeetingPractice;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MeetingPracticeRepository {
+// Spring Data JPA에서 제공하는 JpaRepository interface를 상속받으면
+// @Repository 어노테이션 추가 안해도 됨
+public interface MeetingPracticeRepository extends JpaRepository<MeetingPractice, Integer> {
+    // SpringBoot JpaRepository 인터페이스가 제공하는 Entity의 기본적인 CRUD 메소드를 사용
 }
--- backend/springboot/src/main/java/com/tak/soda/service/MeetingPracticeService.java
+++ backend/springboot/src/main/java/com/tak/soda/service/MeetingPracticeService.java
@@ -1,4 +1,6 @@
 package com.tak.soda.service;
 
 public class MeetingPracticeService {
+
+
 }
