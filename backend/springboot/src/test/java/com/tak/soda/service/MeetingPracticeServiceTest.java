0dabc6feb9bb7bf8c1a895b0a2160685c29ff5ee
TAEJIN <taejin0323@naver.com>
2021-05-15 02:27:20 +0900

    fix: 🐛 meetingPractice

--- backend/springboot/src/test/java/com/tak/soda/service/MeetingPracticeServiceTest.java
+++ backend/springboot/src/test/java/com/tak/soda/service/MeetingPracticeServiceTest.java
@@ -1,9 +1,14 @@
 package com.tak.soda.service;
 
+import com.tak.soda.domain.MeetingPractice;
+import com.tak.soda.repository.MeetingPracticeRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,13 +16,47 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class MeetingPracticeServiceTest {
 
-    @DisplayName("모의면접방 생성")
-    @Test
-    void TestcreateRoom() {
-    }
+    @Autowired MeetingPracticeService meetingPracticeService;
+    @Autowired MeetingPracticeRepository meetingPracticeRepository;
 
     @DisplayName("모의면접방 생성")
     @Test
-    void deleteRoom() {
+    void testCreateRoom() {
+        //Given
+
+        //When
+        MeetingPractice room = meetingPracticeService.createRoom();
+
+        //Then
+        assertEquals(room, meetingPracticeRepository.findByName(room.getName()));
+    }
+
+    @DisplayName("검색")
+    @Test
+    void testFindByName() {
+        //Given
+        String name = "6gCfJehg0i";
+
+        //When
+        boolean success = meetingPracticeService.findByName(name);
+
+        //Then
+        assertEquals(true, success);
+    }
+
+
+    @DisplayName("모의면접방 삭제")
+    @Test
+    void testDeleteRoom() {
+        //Given
+        MeetingPractice room = meetingPracticeService.createRoom();
+        Long mp_id = room.getId();
+
+        System.out.println(mp_id);
+        //When
+        meetingPracticeService.deleteRoom(mp_id);
+
+        //Then
+        assertEquals(Optional.empty(), meetingPracticeRepository.findById(mp_id));
     }
 }
